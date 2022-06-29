fbfa14ce3eb151b952ed982592fe9b041b5a66e5
ashishbinu <ashishbinu90@gmail.com>
2022-06-29 13:59:30 +0530

    wip(login): adding stateful logic

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,13 @@
-import Button from './component/Button.jsx'
+import { useEffect, useState } from "react";
+import Button from "./component/Button.jsx";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  useEffect(() => {
+    setUsername("ahis");
+  }, []);
   return (
     <section>
       <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
@@ -34,6 +40,13 @@ function Login() {
                       required=""
                       placeholder="Your Email"
                       className="block w-full transform rounded-lg border border-transparent bg-gray-50 px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                      value={username}
+                      onChange={(event) => {
+                        setUsername((u) => {
+                          console.log(event.target.value);
+                          return event.target.value;
+                        });
+                      }}
                     />
                   </div>
                 </div>
@@ -54,6 +67,13 @@ function Login() {
                       required=""
                       placeholder="Your Password"
                       className="block w-full transform rounded-lg border border-transparent bg-gray-50 px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                      value={password}
+                      onChange={(event) => {
+                        setPassword((u) => {
+                          console.log(event.target.value);
+                          return event.target.value;
+                        });
+                      }}
                     />
                   </div>
                 </div>
@@ -90,8 +110,8 @@ function Login() {
           </div>
         </div>
       </div>
-    </section >
-  )
+    </section>
+  );
 }
 
 export default Login;
