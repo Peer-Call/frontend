88ec2c2dbfdfb527b85177599615d3eb0c109e13
ashishbinu <ashishbinu90@gmail.com>
2022-07-04 22:02:34 +0530

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
 import Oncall from "./Oncall";
-import PeerCall from "./PeerCall.jsx";
-
+import PeerCall from "./PeerCall";
 
 function App() {
   return (
     <div className="h-screen flex justify-center items-center overflow-hidden">
-      <PeerCall/>
+      <Routes>
+        <Route path="/" element={<PeerCall />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="signin" element={<Login />} />
+        <Route path="videocall" element={<Oncall />} />
+      </Routes>
     </div>
   );
 }
