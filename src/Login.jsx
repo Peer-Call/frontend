c796b3b92d6bf1f4ed63b67710b236c19c483e3b
ashishbinu <ashishbinu90@gmail.com>
2022-07-05 11:17:07 +0530

    why is gun not working

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,85 @@
 import { useEffect, useState } from "react";
 import Button from "./component/Button.jsx";
+import useGunContext from './useGunContext';
+
+// const APP_PUBLIC_KEY = process.env.APP_PUBLIC_KEY;
 
 function Login() {
+  const { getGun, getUser, setCertificate } = useGunContext();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [authError, setAuthError] = useState();
+
+  const logIn = () => {
+    getUser().auth(username, password, ({ err }) => {
+      if (err) {
+        setAuthError(err);
+      }
+    });
+  };
+
+  const onCreateSuccess = ({ pub }) => {
+    // get certificate and store in app memory
+    fetch('http://localhost:8765/api/certificates', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        pub,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then(({ certificate }) => {
+        setCertificate(certificate);
+
+        // add user to user/profile list
+        getGun()
+          .get(`~${APP_PUBLIC_KEY}`)
+          .get('profiles')
+          .get(pub)
+          .put({ username }, null, {
+            opt: { cert: certificate },
+          });
+
+        // log in
+        logIn();
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setAuthError();
+    logIn();
+  };
+
+  const handleSignUp = () => {
+    setAuthError();
+
+    // check if user with username already exists
+    getGun()
+      .get(`~@${username}`)
+      .once((user) => {
+        if (user) {
+          setAuthError('Username already taken');
+        } else {
+          getUser().create(username, password, ({ err, pub }) => {
+            if (err) {
+              setAuthError(err);
+            } else {
+              onCreateSuccess({ pub });
+            }
+          });
+        }
+      });
+  };
 
   useEffect(() => {
-    setUsername("ahis");
+
+    setUsername("ahis@gmail.com");
+    setPassword("ahis@gmail.com");
   }, []);
   return (
     <section>
@@ -22,7 +95,7 @@ function Login() {
           </div>
           <div className="mt-8">
             <div className="mt-6">
-              <form action="#" method="POST" className="space-y-6">
+              <form onSubmit={handleSubmit} >
                 <div>
                   <label
                     htmlFor="email"
