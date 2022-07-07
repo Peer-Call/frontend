7dd8b1a602b33285d22a5071ad1f3a03f3ec1a39
ashishbinu <ashishbinu90@gmail.com>
2022-07-07 11:53:24 +0530

    chore: add official gun relay peer

--- src/GunInstance.js
+++ src/GunInstance.js
@@ -1,13 +1,16 @@
-import GUN from 'gun/gun';
-import 'gun/sea';
+import GUN from "gun/gun";
+import "gun/sea";
 
 const db = GUN({
-  peers: ['http://peercall-gun.herokuapp.com/gun'],
+  peers: [
+    "https://gunjs.herokuapp.com/gun",
+    "http://peercall-gun.herokuapp.com/gun",
+  ],
   localStorage: false,
-  retry: Infinity
+  retry: Infinity,
 });
 
-const user = db.user()
+const user = db.user();
 
 // Usage: use import gunInstance from './GunInstance.js'
 export { db, user };
